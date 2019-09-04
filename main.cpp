3daf52019ff64de8d03451d987105456a5960058
josemorista <josemorista@id.uff.br>
2019-09-04 19:09:53 -0300

    process static imgs

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@
 #include <stdio.h>
 #include <opencv2/imgcodecs.hpp>
 #include <math.h>
+#include <string>
 
 // Consts
-#define realRadious 0.3 // This value represents our objects real radious
-#define focal 450 // This value represents our camera focus
+#define realRadious 1.25 // This value represents our objects real radious
+#define focal 1					 // This value represents our camera focus
 #define minimumArea 1000 // this represents the minimum area to be considered an object
 
 using namespace cv;
@@ -16,75 +17,100 @@ using namespace std;
 // Here comes our classes
 class TrackingObject
 {
-	public:
+public:
+	int lowH, lowS, lowV, highH, highS, highV;
+	int x, y;
+	double distanceFromCamera, realR, R, minArea;
 
-		int lowH, lowS, lowV, highH, highS, highV;
-		int x, y;
-		double distanceFromCamera, realR, R, minArea;
+	TrackingObject(double realR = realRadious, double minArea = minimumArea)
+	{
+		this->x = 0;
+		this->y = 0;
+		this->R = 0;
+		this->realR = realR;
+		this->minArea = minArea;
+	}
 
-		TrackingObject (double realR = realRadious, double minArea = minimumArea) {
-			this->x = 0;
-			this->y = 0;
-			this->R = 0;
-			this->realR = realR;
-			this->minArea = minArea;
+	void setHsvFilter(int lowH, int lowS, int lowV, int highH, int highS, int highV)
+	{
+		this->lowH = lowH;
+		this->lowS = lowS;
+		this->lowV = lowV;
+
+		this->highH = highH;
+		this->highS = highS;
+		this->highV = highV;
+	}
+
+	void calculateCoordinatesWithMoments(Moments m)
+	{
+
+		double dM01 = m.m01;
+		double dM10 = m.m10;
+		double dArea = m.m00;
+
+		// Check the min area
+		if (dArea < this->minArea)
+			return;
+
+		// Calculate the centroid of the object
+		this->x = dM10 / dArea;
+		this->y = dM01 / dArea;
+
+		// Calculate our object radious
+		this->R = sqrt((dArea / 255) / 3.14);
+
+		// Calculate the distance between the Object and the camera
+		this->distanceFromCamera = (focal * this->realR) / this->R;
+	}
+
+	void drawCircle(cv::Mat &img, Scalar color = Scalar(0, 255, 0))
+	{
+		if (this->x >= 0 && this->y >= 0)
+		{
+			circle(img, Point(this->x, this->y), this->R, color, 2);
 		}
+	}
 
-		void setHsvFilter (int lowH, int lowS, int lowV, int highH, int highS, int highV) {
-			this->lowH=lowH;
-			this->lowS=lowS;
-			this->lowV=lowV;
+	void drawLineBetweenObject(cv::Mat &img, TrackingObject obj, Scalar color = Scalar(255, 0, 0))
+	{
+		line(img, Point(this->x, this->y), Point(obj.x, obj.y), color, 2);
+	}
 
-			this->highH=highH;
-			this->highS=highS;
-			this->highV=highV;
+	double calculateDistanceBetweenObjct(TrackingObject obj)
+	{
+		if (this->R != 0 && obj.R != 0)
+		{
+			double distanceBetweenPx = sqrt(pow((obj.x - this->x), 2) + pow((obj.y - this->y), 2));
+			double distanceBetweenCm = distanceBetweenPx * this->distanceFromCamera / focal;
+			return distanceBetweenCm;
 		}
-
-		void calculateCoordinatesWithMoments (Moments m) {
-			
-			double dM01 = m.m01;
-			double dM10 = m.m10;
-			double dArea = m.m00;
-
-			// Check the min area
-			if (dArea < this->minArea) return;
-
-			// Calculate the centroid of the object
-			this->x = dM10 / dArea;
-			this->y = dM01 / dArea;
-			
-			// Calculate our object radious
-			this->R = sqrt((dArea / 255) / 3.14);
-
-			// Calculate the distance between the Object and the camera
-			this->distanceFromCamera = (focal * this->realR) / this->R;
-		}
-
-		void drawCircle (cv::Mat& img, Scalar color = Scalar(0, 255, 0)) {
-			if (this->x >= 0 && this->y >= 0) {
-				circle(img, Point(this->x, this->y), this->R, color, 2);
-			}
-		}
-
-		void drawLineBetweenObject (cv::Mat& img, TrackingObject obj, Scalar color = Scalar(255, 0, 0)) {
-			line(img, Point(this->x, this->y), Point(obj.x, obj.y), color, 2);
-		}
-
-		double calculateDistanceBetweenObjct (TrackingObject obj) {
-			if (this->R != 0 && obj.R != 0)
-			{
-				double distanceBetweenPx = sqrt(pow((obj.x - this->x), 2) + pow((obj.y - this->y), 2));
-				double distanceBetweenCm = distanceBetweenPx * this->distanceFromCamera / focal;
-				return distanceBetweenCm;
-			}
-			return 0;
-		}
-
+		return 0;
+	}
 };
 
 // Finally our main arrives!
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
+
+	String imgPath;
+
+	if (argc > 1)
+	{
+		imgPath = argv[1];
+	}
+	else
+	{
+		char resp;
+		cout << "Do you want to process a static img?(y/n)" << endl;
+		cin >> resp;
+		if (resp == 'y')
+		{
+			cout << "Type me the img path:" << endl;
+			cin >> imgPath;
+		}
+	}
+
 	// Video capture variable
 	VideoCapture cap(0);
 
@@ -94,7 +120,6 @@ int main (int argc, char **argv)
 		return -1;
 	}
 
-	
 	// Our objects
 	TrackingObject initialObj, finalObj;
 
@@ -103,8 +128,8 @@ int main (int argc, char **argv)
 	namedWindow("Controlers for finalHSVfilter");
 
 	// Here we choose the interval to get the objects by their HSV color, see the hsvMap to get your desired values!
-	initialObj.setHsvFilter(100, 76, 0, 130, 255, 255);
-	finalObj.setHsvFilter(28, 69, 0, 35, 255, 255);
+	initialObj.setHsvFilter(160, 70, 0, 180, 255, 255);
+	finalObj.setHsvFilter(2, 107, 0, 33, 255, 255);
 
 	// Create Trackbars
 	createTrackbar("LowH", "Controlers for initialHSVfilter", &initialObj.lowH, 179);
@@ -122,7 +147,15 @@ int main (int argc, char **argv)
 
 		Mat imgOriginal;
 		// Capture image from camera
-		cap.read(imgOriginal);
+
+		if (imgPath.length() > 0)
+		{
+			imgOriginal = imread(imgPath);
+		}
+		else
+		{
+			cap.read(imgOriginal);
+		}
 
 		Mat imgHSV;
 		// Convert the captured frame from BGR to HSV
@@ -135,8 +168,10 @@ int main (int argc, char **argv)
 		inRange(imgHSV, Scalar(finalObj.lowH, finalObj.lowS, finalObj.lowV), Scalar(finalObj.highH, finalObj.highS, finalObj.highV), fThresholded);
 
 		// Erode the images to remove some noise
-		erode(iThresholded, iThresholded, getStructuringElement(MORPH_RECT, Size(5, 5)));
-		erode(fThresholded, fThresholded, getStructuringElement(MORPH_RECT, Size(5, 5)));
+		erode(iThresholded, iThresholded, getStructuringElement(MORPH_RECT, Size(8, 8)));
+		dilate(iThresholded, iThresholded, getStructuringElement(MORPH_RECT, Size(8, 8)));
+		erode(fThresholded, fThresholded, getStructuringElement(MORPH_RECT, Size(8, 8)));
+		dilate(fThresholded, fThresholded, getStructuringElement(MORPH_RECT, Size(8, 8)));
 
 		// Calculate the Moments of the Thresholdeds Images
 		Moments iMoments = moments(iThresholded);
@@ -145,7 +180,7 @@ int main (int argc, char **argv)
 		// Calculate what we need with the moments
 		initialObj.calculateCoordinatesWithMoments(iMoments);
 		finalObj.calculateCoordinatesWithMoments(fMoments);
-		
+
 		// Draw the two circles
 		initialObj.drawCircle(imgOriginal);
 		finalObj.drawCircle(imgOriginal);
@@ -154,11 +189,18 @@ int main (int argc, char **argv)
 		initialObj.drawLineBetweenObject(imgOriginal, finalObj);
 
 		// Calcultate the distance between the objects
-		double distance = initialObj.calculateDistanceBetweenObjct(finalObj);
-		if (distance > 0) cout << "Distance between = " << distance << "cm" << endl;
-		
+		double distance = finalObj.calculateDistanceBetweenObjct(initialObj);
+		if (distance > 0)
+			cout << "Distance between = " << distance << "cm" << endl;
+
 		// Show result to user
 		imshow("Result!", imgOriginal);
+
+		if (imgPath.length() > 0)
+		{
+			cout << "processed " << imgPath << endl;
+			// return 0;
+		}
 
 		// Wait for key is pressed then break loop
 		if (waitKey(5) == 27) //ESC == 27
